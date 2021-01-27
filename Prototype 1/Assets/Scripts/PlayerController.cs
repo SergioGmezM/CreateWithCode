eda2438750df627ac3cfa992ef66c03944e94768
TheBender <i62gomos@uco.es>
2021-01-27 10:57:47 +0100

    Implemented player input control to the car and basic physics

--- Prototype 1/Assets/Scripts/PlayerController.cs
+++ Prototype 1/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    float ACCELERATION_THRESHOLD = 0.01f;
+
+    public float speed = 15.0f;
+    public float turnSpeed = 35.0f;
+
+    private int isAccelerating;
+    private float horizontalInput;
+    private float forwardInput;
+
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
+        if(forwardInput > ACCELERATION_THRESHOLD || forwardInput < -ACCELERATION_THRESHOLD)
+        {
+            isAccelerating = 1;
+        } else
+        {
+            isAccelerating = 0;
+        }
+
+        transform.Translate(Vector3.forward * speed * forwardInput * Time.deltaTime);
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime * isAccelerating);
     }
 }
