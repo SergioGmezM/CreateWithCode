28323b69d0e281d1c39d77b8c97b53fb09e8b8ca
TheBender10 <sergiogomez9867@gmail.com>
2021-02-03 09:14:15 +0100

    Animals randomly spawn at different locations

---
+++ Prototype 2/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject[] animalPrefabs;
+    private float spawnX = 15.0f;
+    private float spawnZ = 20.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            int indexPrefab = Random.Range(0, animalPrefabs.Length);
+            Vector3 spawnPos = new Vector3(Random.Range(-spawnX, spawnX), 0, spawnZ);
+            Instantiate(animalPrefabs[indexPrefab], spawnPos, animalPrefabs[indexPrefab].transform.rotation);
+        }        
+    }
+}
