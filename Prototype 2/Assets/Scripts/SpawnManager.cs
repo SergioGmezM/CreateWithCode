0975e47acee7ae49b451a932136ffdf202d69070
TheBender10 <sergiogomez9867@gmail.com>
2021-02-03 10:03:16 +0100

    Added collision interaction between animals and food

--- Prototype 2/Assets/Scripts/SpawnManager.cs
+++ Prototype 2/Assets/Scripts/SpawnManager.cs
@@ -5,23 +5,27 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject[] animalPrefabs;
+
     private float spawnX = 15.0f;
     private float spawnZ = 20.0f;
+    private float startingTime = 2.0f;
+    private float spawnInterval = 1.5f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("SpawnRandomAnimal", startingTime, spawnInterval);
     }
 
     // Update is called once per frame
     void Update()
+    {     
+    }
+
+    void SpawnRandomAnimal()
     {
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            int indexPrefab = Random.Range(0, animalPrefabs.Length);
-            Vector3 spawnPos = new Vector3(Random.Range(-spawnX, spawnX), 0, spawnZ);
-            Instantiate(animalPrefabs[indexPrefab], spawnPos, animalPrefabs[indexPrefab].transform.rotation);
-        }        
+        int indexPrefab = Random.Range(0, animalPrefabs.Length);
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnX, spawnX), 0, spawnZ);
+        Instantiate(animalPrefabs[indexPrefab], spawnPos, animalPrefabs[indexPrefab].transform.rotation);
     }
 }
