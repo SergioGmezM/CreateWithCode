1f1ed6f014ed43f17bd0ab3d97f3a77210bf770a
TheBender10 <sergiogomez9867@gmail.com>
2021-02-12 09:56:36 +0100

    Added player controls

---
+++ Prototype 4/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 200.0f;
+
+    private Rigidbody playerRB;
+    private GameObject focalPoint;
+    private float forwardInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRB = GetComponent<Rigidbody>();
+        focalPoint = GameObject.Find("Camera Focal Point");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        forwardInput = Input.GetAxis("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        playerRB.AddForce(focalPoint.transform.forward * speed * forwardInput);
+        playerRB.AddForce(focalPoint.transform.forward * speed * -forwardInput / 2);
+    }
+}
